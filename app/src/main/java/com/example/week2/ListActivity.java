494fddf1cf035ce830784f7bcc3654226a02ed34
IceFire246 <39646256+IceFire246@users.noreply.github.com>
2023-05-29 10:01:33 +0800

    ItemAdapter class + updates

--- app/src/main/java/com/example/week2/ListActivity.java
+++ app/src/main/java/com/example/week2/ListActivity.java
@@ -1,16 +1,24 @@
 package com.example.week2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
@@ -40,8 +48,69 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        ImageView icon = (ImageView) this.findViewById(R.id.imgIcon);
-        icon.setOnClickListener(new View.OnClickListener() {
+        ArrayList<User> userList = new ArrayList<>();
+
+
+        for (int i = 0; i < 20; i++) {
+            Random r = new Random();
+            String numName = "Name" + r.nextInt();
+            String numDesc = "Description "+ r.nextInt();
+            boolean follow = r.nextBoolean();
+
+            User u = new User(numName, numDesc, follow);
+            userList.add(u);
+        }
+
+        class UserViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            TextView desc;
+            ImageView img;
+            public UserViewHolder(View itemView) {
+                super(itemView);
+                name = itemView.findViewById(android.R.id.text1);
+                desc = itemView.findViewById(android.R.id.text2);
+                img = itemView.findViewById(android.R.id.icon);
+            }
+        }
+
+        class UserAdapter extends
+                RecyclerView.Adapter<UserViewHolder>{
+            ArrayList<User> data;
+            public UserAdapter(ArrayList<User> input) {
+                data = input;
+            }
+            public UserViewHolder onCreateViewHolder(
+                    ViewGroup parent,
+                    int viewType) {
+                View item = LayoutInflater.from(parent.getContext()).inflate(
+                        android.R.layout.simple_list_item_1,
+                        parent,
+                        false);
+                return new UserViewHolder(item);
+            }
+            public void onBindViewHolder(
+                    UserViewHolder holder,
+                    int position) {
+                User u = data.get(position);
+                holder.name.setText(u.name);
+                holder.desc.setText(u.description);
+                holder.itemView.findViewById(R.id.mipmap_ic_launcher_round);
+            }
+            public int getItemCount() {
+                return data.size();
+            }
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        UserAdapter mAdapter = new UserAdapter(userList);
+        LinearLayoutManager mLayoutManager =
+                new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+
+        recyclerView.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v)
             {
--- app/src/main/java/com/example/week2/User.java
+++ app/src/main/java/com/example/week2/User.java
@@ -5,6 +5,12 @@ public class User {
     public String description;
     public int id;
     public static boolean followed;
+
+    public User(String n, String desc, boolean follow) {
+        name = n;
+        description = desc;
+        followed = follow;
+    }
 }
 
 
